b08c6f5fdf8e88741eadb954c53a084c7d2b3e7c
ackphht <ackphht@outlook.com>
2023-03-19 15:37:20 -0500

    getSystemInformation: change _charsToString to just output hex codes instead of trying to do the fancy chars; they're always too hard to read

--- getSystemInformation.ps1
+++ getSystemInformation.ps1
@@ -395,12 +395,19 @@ function _parseLinuxLines {
 	return ''
 }
 
-$script:nonDisplayCharsMap = @{
+<# $script:nonDisplayCharsMap = @{
 	[char]0 = '␀'; [char]1 = '␁'; [char]2 = '␂'; [char]3 = '␃'; [char]4 = '␄'; [char]5 = '␅'; [char]6 = '␆'; [char]7 = '␇';
 	[char]8 = '␈'; [char]9 = '␉'; [char]0x0a = '␊'; [char]0x0b = '␋'; [char]0x0c = '␌'; [char]0x0d = '␍'; [char]0x0e = '␎'; [char]0x0f = '␏'; [char]0x10 = '␐';
 	[char]0x11 = '␑'; [char]0x12 = '␒'; [char]0x13 = '␓'; [char]0x14 = '␔'; [char]0x15 = '␕'; [char]0x16 = '␖'; [char]0x17 = '␗';
 	[char]0x18 = '␘'; [char]0x19 = '␙'; [char]0x1a = '␚'; [char]0x1b = '␛'; [char]0x1c = '␜'; [char]0x1d = '␝'; [char]0x1e = '␞'; [char]0x1f = '␟';
-	[char]0x20 = '␠'; [char]0x7f = '␡'; [char]0xa0<#NBSP#> = ' '; [char]0x85<#NEL#> = ' '; [char]0x2028<#LS#> = ' '; [char]0x2029<#PS#> = ' ';
+	[char]0x20 = '␠'; [char]0x7f = '␡'; [char]0xa0<#NBSP#><# = ' '; [char]0x85<#NEL#><# = ' '; [char]0x2028<#LS#><# = ' '; [char]0x2029<#PS#><# = ' ';
+} #>
+$script:nonDisplayCharsMap = @{
+	[char]0 = '\x00'; [char]1 = '\x01'; [char]2 = '\x02'; [char]3 = '\x03'; [char]4 = '\x04'; [char]5 = '\x05'; [char]6 = '\x05'; [char]7 = '\x07';
+	[char]8 = '\b'; [char]9 = '\t'; [char]0x0a = '\n'; [char]0x0b = '\x0b'; [char]0x0c = '\f'; [char]0x0d = '\r'; [char]0x0e = '\x0e'; [char]0x0f = '\x0f';
+	[char]0x10 = '\x10'; [char]0x11 = '\x11'; [char]0x12 = '\x12'; [char]0x13 = '\x13'; [char]0x14 = '\x14'; [char]0x15 = '\x15'; [char]0x16 = '\x16'; [char]0x17 = '\x17';
+	[char]0x18 = '\x18'; [char]0x19 = '\x19'; [char]0x1a = '\x1a'; [char]0x1b = '\x1b'; [char]0x1c = '\x1c'; [char]0x1d = '\x1d'; [char]0x1e = '\x1e'; [char]0x1f = '\x1f';
+	[char]0x20 = '␠'; [char]0x7f = '\x7f'; [char]0xa0<#NBSP#> = '\xa0'; [char]0x85<#NEL#> = '\x85'; [char]0x2028<#LS#> = '\u2028'; [char]0x2029<#PS#> = '\u2029';
 }
 
 function _charsToString {
@@ -411,7 +418,8 @@ function _charsToString {
 	)
 	if ($chars) {
 		$sb = [System.Text.StringBuilder]::new(512)
-		foreach ($c in $chars) {
+		foreach ($c in ($chars | Sort-Object)) {
+			if ($sb.Length -gt 0) { [void]$sb.Append(' ') }
 			if ($script:nonDisplayCharsMap.ContainsKey($c)) {
 				[void]$sb.Append($script:nonDisplayCharsMap[$c])
 			} else {
