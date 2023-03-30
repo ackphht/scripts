66e679092e4590f60d3a595b7b56aab40fd7d33d
ackphht <ackphht@outlook.com>
2023-03-29 21:16:42 -0500

    getComputerInfo: add check that running on Windows

--- getComputerInfo.ps1
+++ getComputerInfo.ps1
@@ -5,8 +5,15 @@ $script:bigDivider = [string]::new('=', 80)
 $script:smallDivider = [string]::new('-', 80)
 
 . $PSScriptRoot/helpers.ps1
+Set-StrictMode -Off # helpers.ps1 above is turning it on
 
 function Main {
+	# make sure we're on Windows; can use $IsWindows; if $IsWindows is not defined, then it's non-Core powershell, which is Windows only
+	if (([bool](Get-Variable -Name 'IsWindows' -ErrorAction Ignore) -and -not $IsWindows)) {
+		Write-Error 'this script is for Windows only'
+		return
+	}
+
 	WriteHeader 'Get-ComputerInfo'
 	$ci = Get-ComputerInfo
 	$ci | Format-List (GetSortedPropertyNames $ci)
