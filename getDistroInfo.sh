9743565f582e1f9926aa687873b8e691136e3754
ackphht <ackphht@outlook.com>
2023-03-31 13:00:46 -0500

    getDistroInfo: add sw_vers for mac

--- getDistroInfo.sh
+++ getDistroInfo.sh
@@ -19,20 +19,21 @@ test -f /etc/linuxmint/info && cp /etc/linuxmint/info linuxmint_info
 test -f /etc/mx-version && cp /etc/mx-version .
 
 # sysctl needs sudo to access everything it wants, but also on some OSes (e.g. opensuse) need sudo just to see it:
-sudo which sysctl > /dev/null 2>&1 && sudo sysctl -a | sort --ignore-case > sysctl.log || echo "WARNING: sysctl not found"
-which lsb_release > /dev/null 2>&1 && lsb_release -a > lsb_release.log 2>/dev/null || echo "WARNING: lsb_release not found"
-which screenfetch > /dev/null 2>&1 && screenfetch -N > screenfetch.log 2>/dev/null || echo "WARNING: screenfetch not found"
-which neofetch > /dev/null 2>&1 && neofetch --stdout > neofetch.log 2>/dev/null || echo "WARNING: neofetch not found"
+sudo which sysctl >/dev/null 2>&1 && sudo sysctl -a | sort --ignore-case > sysctl.log || echo "WARNING: sysctl not found"
+which lsb_release >/dev/null 2>&1 && lsb_release -a > lsb_release.log 2>/dev/null || echo "WARNING: lsb_release not found"
+which screenfetch >/dev/null 2>&1 && screenfetch -N > screenfetch.log 2>/dev/null || echo "WARNING: screenfetch not found"
+which neofetch >/dev/null 2>&1 && neofetch --stdout > neofetch.log 2>/dev/null || echo "WARNING: neofetch not found"
 
 test -f $scriptRoot/showSomeProps.py && python3 $scriptRoot/showSomeProps.py > showSomeProps.py.log
 
-which pwsh > /dev/null 2>&1 && test -f $scriptRoot/getSystemInformation.ps1 && \
+which pwsh >/dev/null 2>&1 && test -f $scriptRoot/getSystemInformation.ps1 && \
 	pwsh -command "& { $scriptRoot/getSystemInformation.ps1 | Out-File getSystemInformation.log -Width 4096 }" && \
 	pwsh -command "& { $scriptRoot/getSystemInformation.ps1 -asCsv }"
 
 # for macOS:
-which system_profiler > /dev/null 2>&1 && system_profiler -json SPHardwareDataType SPSoftwareDataType SPMemoryDataType SPStorageDataType SPNVMeDataType > system_profiler.json && \
+which system_profiler >/dev/null 2>&1 && system_profiler -json SPHardwareDataType SPSoftwareDataType SPMemoryDataType SPStorageDataType SPNVMeDataType > system_profiler.json && \
 	system_profiler SPHardwareDataType SPSoftwareDataType SPMemoryDataType SPStorageDataType SPNVMeDataType > system_profiler.log
+which sw_vers >/dev/null 2>&1 && sw_vers > sw_vers.log || true
 
 #uname -a > uname.txt
 echo -n '' > uname.log
