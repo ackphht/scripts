03b376d845796880eca01b67d389e0786a3d8977
ackphht <ackphht@outlook.com>
2023-04-04 14:00:53 -0500

    showPaths.ps1: add PythonPath; refactor to make displaying values a function to make it easier to add more, if ever

--- showPaths.ps1
+++ showPaths.ps1
@@ -1,14 +1,26 @@
-﻿$dividerColor = [System.ConsoleColor]::DarkCyan
+﻿$headerColor = [System.ConsoleColor]::DarkCyan
 $outputColor = [System.ConsoleColor]::Green
-$dividerChar = '*'
-$bigDivider = [string]::new($dividerChar, 80)
+$headerChar = '*'; $headerWidth = 80;
+$bigDivider = [string]::new($headerChar, $headerWidth)
 
-Write-Host $bigDivider -ForegroundColor $dividerColor
-Write-Host ('{0}{0}{1} {2} {1}{0}{0}' -f $dividerChar,(' ' * 35),'Path') -ForegroundColor $dividerColor
-Write-Host $bigDivider -ForegroundColor $dividerColor
-Write-Host ($env:PATH -replace [System.IO.Path]::PathSeparator,[System.Environment]::NewLine) -ForegroundColor $outputColor
+function showPath {
+	param([string] $name, [string] $value)
+	if ($value) {
+		$totalSpaces = $headerWidth - 6 <# four '*'s, two spaces #> - $name.Length
+		$halfSpaces = [int][Math]::Floor($totalSpaces / 2)
+		if (($totalSpaces % 2) -eq 0) {
+			$preSpaces = $postSpaces = [string]::new(' ', $halfSpaces)
+		} else {
+			$preSpaces = [string]::new(' ', $halfSpaces)
+			$postSpaces = [string]::new(' ', $halfSpaces + 1)
+		}
+		Write-Host $bigDivider -ForegroundColor $headerColor
+		Write-Host ('{0}{0}{1} {2} {3}{0}{0}' -f $headerChar,$preSpaces,$name,$postSpaces) -ForegroundColor $headerColor
+		Write-Host $bigDivider -ForegroundColor $headerColor
+		Write-Host ($value -replace [System.IO.Path]::PathSeparator,[System.Environment]::NewLine) -ForegroundColor $outputColor
+	}
+}
 
-Write-Host $bigDivider -ForegroundColor $dividerColor
-Write-Host ('{0}{0}{1} {2} {1}{0}{0}' -f $dividerChar,(' ' * 31),'PSModulePath') -ForegroundColor $dividerColor
-Write-Host $bigDivider -ForegroundColor $dividerColor
-Write-Host ($env:PSModulePath -replace [System.IO.Path]::PathSeparator,[System.Environment]::NewLine) -ForegroundColor $outputColor
+showPath -name 'Path' -value $env:PATH
+showPath -name 'PSModulePath' -value $env:PSModulePath
+showPath -name 'PythonPath' -value $env:PYTHONPATH
