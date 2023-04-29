6b4d97506e4a1215f0b4804443a1b4cad85ebcd5
ackphht <ackphht@outlook.com>
2023-04-29 13:13:13 -0500

    ackShellStuff: add some more helper package manager aliases

--- ackShellStuff.sh
+++ ackShellStuff.sh
@@ -21,27 +21,53 @@ alias cj='sudo journalctl --vacuum-time=1d'
 
 if type -p apt >/dev/null; then
 	alias aptr='sudo apt update'
-	alias aptl='apt list --upgradable'
+	alias aptul='apt list --upgradable'
 	alias aptu='sudo apt upgrade --yes'
 	alias aptc='sudo apt-get autoremove --yes && sudo apt-get autoclean --yes && sudo apt-get clean --yes'
+	alias apts='apt-cache search'
+	alias aptn='apt show'
+	alias apti='sudo apt install'
+	alias aptx='sudo apt remove'	# leaves settings	(leaving off --yes)
+	alias aptxx='sudo apt purge'	# removes settings too
+	alias aptl='apt list --installed'
 elif type -p dnf >/dev/null; then
 	alias aptr='sudo dnf check-update --refresh'
-	alias aptl='sudo dnf check-update'	# ???
+	alias aptul='sudo dnf check-update'	# ???
 	alias aptu='sudo dnf upgrade --assumeyes'
 	#alias aptc='sudo dnf autoremove --assumeyes --cacheonly && sudo dnf clean all --assumeyes --cacheonly'
 	alias aptc='sudo dnf autoremove --assumeyes && sudo dnf clean all --assumeyes --cacheonly'
 	#alias aptc='sudo dnf autoremove --assumeyes --cacheonly && pkcon refresh force --cache-age -1 && sudo dnf clean all --assumeyes --cacheonly'
+	alias apts='dnf search'
+	alias aptn='dnf info'
+	alias apti='sudo dnf install'
+	alias aptx='sudo dnf remove'	# these both do the same thing
+	alias aptxx='sudo dnf remove'	# but to keep the same aliases available...
+	alias aptl='dnf list --installed'
 elif type -p zypper >/dev/null; then
-	alias aptr='sudo zypper refresh --force'
-	alias aptl='zypper list-updates'
+	alias aptr='sudo zypper refresh --force'	# if output is piped into, e.g. grep, it displays a warning about not having a 'stable CLI interface', 'use with caution'; ???
+	alias aptul='zypper list-updates'
 	alias aptu='sudo zypper update --no-confirm'
 	#alias aptc='sudo zypper remove --clean-deps && sudo zypper clean --all'
 	alias aptc='sudo zypper clean --all'
+	alias apts='zypper search'
+	alias aptn='zypper info'
+	alias apti='sudo zypper install'
+	alias aptx='sudo zypper remove --clean-deps'	# these both do the same thing
+	alias aptxx='sudo zypper remove --clean-deps'	# but to keep the same aliases available...
+	alias aptl='zypper packages --installed-only'
 elif type -p pacman >/dev/null; then
 	alias aptr='sudo pacman --sync --refresh'
-	alias aptl='pacman --query --upgrades'
+	alias aptul='pacman --query --upgrades'
 	alias aptu='sudo pacman --sync --sysupgrade --noconfirm'
 	alias aptc='sudo pacman --sync --clean --noconfirm'
+	alias apts='pacman --sync --search'
+	alias aptn='pacman --query --info'
+	alias apti='sudo pacman --sync'
+	alias aptx='sudo pacman --remove --recursive'
+	alias aptxx='sudo pacman --remove --recursive --nosave'
+	alias aptl='pacman --query'
+	# just to store this somewhere: to list all explicitly installed packages that aren't required by something else:
+	#	pacman --query --explicit --unrequired (or pacman -Qet if wanna be lazy)
 fi
 
 type -p btrfs >/dev/null && alias defrag='sudo btrfs filesystem defrag -czstd -rv /' || true
