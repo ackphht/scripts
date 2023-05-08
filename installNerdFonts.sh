6d547f5f1927bb35824013a4cbec8a2c29a95ed3
ackphht <ackphht@outlook.com>
2023-05-08 12:22:41 -0500

    installNerdFonts.sh: rework for v3

--- installNerdFonts.sh
+++ installNerdFonts.sh
@@ -5,27 +5,75 @@ if ! $(which wget > /dev/null 2>&1) ; then
 	exit 1
 fi
 
-userFontsFldr=$HOME/.local/share/fonts
+userFontsFldrV2=$HOME/.local/share/fonts
+userFontsFldr=$HOME/.local/share/fonts/NerdFonts
 if [[ ! -d $userFontsFldr ]]; then
 	echo "creating folder '$userFontsFldr'"
 	mkdir -p $userFontsFldr
 fi
 
+cleanUpOldFont() {
+	local filenameV2="${userFontsFldrV2}/$1"
+	local filename="${userFontsFldr}/$1"
+	if [[ -f "$filenameV2" ]]; then
+		echo "removing old font \"$filenameV2\""
+		rm -f "$filenameV2"
+	fi
+	if [[ -f "$filename" ]]; then
+		echo "removing old font \"$filename\""
+		rm -f "$filename"
+	fi
+}
+
+installNerdFont() {
+	local fontname=$1
+	local style=$2
+	local filename=$3
+	local outputName="${userFontsFldr}/$3"
+	# assuming no spaces in the file names anymore:
+	local url="https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/${fontname}/${style}/${filename}"
+
+	echo
+	echo '################################################'
+	echo $filename
+	echo '################################################'
+	wget --output-document $outputName $url
+}
+
+fantasqueFontName='FantasqueSansMono'
+fantasqueFilenameBase='FantasqueSansMNerdFont'
+fantasqueBaseNameV2='Fantasque Sans Mono'
+firaCodeFontName='FiraCode'
+firaCodeFilenameBase='FiraCodeNerdFont'
+firaCodeBaseNameV2='Fira Code'
+
+# remove old fonts (like for <= v2 which had different names):
+cleanUpOldFont "${fantasqueBaseNameV2} Regular Nerd Font Complete.ttf"
+cleanUpOldFont "${fantasqueBaseNameV2} Bold Nerd Font Complete.ttf"
+cleanUpOldFont "${fantasqueBaseNameV2} Italic Nerd Font Complete.ttf"
+cleanUpOldFont "${fantasqueBaseNameV2} Bold Italic Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} Regular Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} Bold Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} Light Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} Medium Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} SemiBold Nerd Font Complete.ttf"
+cleanUpOldFont "${firaCodeBaseNameV2} Retina Nerd Font Complete.ttf"
+
 #
 # download fonts:
-#
 # FantasqueSansMono:
-wget --output-document "$userFontsFldr/Fantasque Sans Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FantasqueSansMono/Regular/complete/Fantasque%20Sans%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fantasque Sans Mono Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FantasqueSansMono/Bold/complete/Fantasque%20Sans%20Mono%20Bold%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fantasque Sans Mono Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FantasqueSansMono/Italic/complete/Fantasque%20Sans%20Mono%20Italic%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fantasque Sans Mono Bold Italic Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FantasqueSansMono/Bold-Italic/complete/Fantasque%20Sans%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+installNerdFont $fantasqueFontName 'Regular' "${fantasqueFilenameBase}-Regular.ttf"
+installNerdFont $fantasqueFontName 'Bold' "${fantasqueFilenameBase}-Bold.ttf"
+installNerdFont $fantasqueFontName 'Italic' "${fantasqueFilenameBase}-Italic.ttf"
+installNerdFont $fantasqueFontName 'Bold-Italic' "${fantasqueFilenameBase}-BoldItalic.ttf"
 # FiraCode:
-wget --output-document "$userFontsFldr/Fira Code Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fira Code Light Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fira Code Medium Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fira Code Retina Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete.ttf
-wget --output-document "$userFontsFldr/Fira Code SemiBold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/SemiBold/complete/Fira%20Code%20SemiBold%20Nerd%20Font%20Complete.ttf
+# now get latest fonts:
+installNerdFont $firaCodeFontName 'Regular' "${firaCodeFilenameBase}-Regular.ttf"
+installNerdFont $firaCodeFontName 'Bold' "${firaCodeFilenameBase}-Bold.ttf"
+installNerdFont $firaCodeFontName 'Light' "${firaCodeFilenameBase}-Light.ttf"
+installNerdFont $firaCodeFontName 'Medium' "${firaCodeFilenameBase}-Medium.ttf"
+installNerdFont $firaCodeFontName 'SemiBold' "${firaCodeFilenameBase}-SemiBold.ttf"
+installNerdFont $firaCodeFontName 'Retina' "${firaCodeFilenameBase}-Retina.ttf"
 
 # update font cache:
 fc-cache -vf
