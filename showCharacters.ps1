8fb2af238919da392c6b4105c4d6f9a5cea2ff7f
ackphht <ackphht@outlook.com>
2023-06-02 15:20:12 -0500

    showCharacters.ps1: add

---
+++ showCharacters.ps1
@@ -0,0 +1,17 @@
+﻿#Requires -Version 5
+
+[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+	[Alias('nf')] [switch] $includeNerdFontChars
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+Set-StrictMode -Version 'Latest'
+
+Write-Output ''
+Write-Output 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789'
+Write-Output '!?@#$%^&* ;: `''"‘’“” ~-_=+ /\|¦ () [] {} <> oO0 iIlL1 g9qCGQ ~-+=>'
+Write-Output 'Áá Ää Åå Ææ Çç Éé Ùù Ïï İı Ññ ß '
+if ($includeNerdFontChars) {
+	Write-Output '<TBD>'
+}
