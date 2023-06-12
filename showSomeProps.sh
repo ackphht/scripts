59f1aab8b5739c3c94891dec573b3ffda70d8c32
ackphht <ackphht@outlook.com>
2023-06-12 13:41:43 -0500

    showSomeProps.sh: use more quotes so don't have to escape as much

--- showSomeProps.sh
+++ showSomeProps.sh
@@ -3,43 +3,43 @@
 has() { type -p "$1" >/dev/null; }
 
 echo
-echo \$0 = \|$0\|
-echo \$SHELL = \|$SHELL\|
-echo \$TERM = \|$TERM\|
-echo \$BASH = \|$BASH\|
-echo \$BASH_VERSION = \|$BASH_VERSION\|
-echo \$ZSH = \|$ZSH\|
-echo \$ZSH_NAME = \|$ZSH_NAME\|
-echo \$ZSH_VERSION = \|$ZSH_VERSION\|
-echo \$HOSTNAME = \|$HOSTNAME\|
-echo \$HOSTTYPE = \|$HOSTTYPE\|
-echo \$MACHTYPE = \|$MACHTYPE\|
-echo \$OSTYPE = \|$OSTYPE\|
+echo "\$0 = |$0|"
+echo "\$SHELL = |$SHELL|"
+echo "\$TERM = |$TERM|"
+echo "\$BASH = |$BASH|"
+echo "\$BASH_VERSION = |$BASH_VERSION|"
+echo "\$ZSH = |$ZSH|"
+echo "\$ZSH_NAME = |$ZSH_NAME|"
+echo "\$ZSH_VERSION = |$ZSH_VERSION|"
+echo "\$HOSTNAME = |$HOSTNAME|"
+echo "\$HOSTTYPE = |$HOSTTYPE|"
+echo "\$MACHTYPE = |$MACHTYPE|"
+echo "\$OSTYPE = |$OSTYPE|"
 if has readlink; then
-	echo \"readlink /proc/\$\$/exe\" = \|$(readlink -f /proc/$$/exe)\|
+	echo "\"readlink /proc/\$\$/exe\" = |$(readlink -f /proc/$$/exe)|"
 else
-	echo \"readlink /proc/\$\$/exe\" = \<n/a\>
+	echo '"readlink /proc/$$/exe" = <n/a>'
 fi
 if has uname; then
-	echo uname: kernel-name [-s] = \|$(uname -s)\|
-	echo uname: kernel-release [-r] = \|$(uname -r)\|
-	echo uname: kernel-version [-v] = \|$(uname -v)\|
-	echo uname: operating system [-o] = \|$(uname -o)\|
-	echo uname: machine [-m] = \|$(uname -m)\|
-	echo uname: processor [-p] = \|$(uname -p 2>/dev/null || echo \<n/a\>)\|
-	echo uname: hardware-platform [-i] = \|$(uname -i 2>/dev/null || echo \<n/a\>)\|
+	echo "uname: kernel-name [-s] = |$(uname -s)|"
+	echo "uname: kernel-release [-r] = |$(uname -r)|"
+	echo "uname: kernel-version [-v] = |$(uname -v)|"
+	echo "uname: operating system [-o] = |$(uname -o)|"
+	echo "uname: machine [-m] = |$(uname -m)|"
+	echo "uname: processor [-p] = |$(uname -p 2>/dev/null || echo \<n/a\>)|"
+	echo "uname: hardware-platform [-i] = |$(uname -i 2>/dev/null || echo \<n/a\>)|"
 else
-	echo uname = \<n/a\>
+	echo 'uname = <n/a>'
 fi
 if has lsb_release; then
 	echo 'lsb_release = |'
 	lsb_release -a 2>/dev/null
 	echo '|'
 else
-	echo lsb_release = \<n/a\>
+	echo 'lsb_release = <n/a>'
 fi
 if has python3; then
-	echo python3 = \|$(python3 --version)\|
+	echo "python3 = |$(python3 --version)|"
 else
-	echo python3 = \<n/a\>
+	echo 'python3 = <n/a>'
 fi
