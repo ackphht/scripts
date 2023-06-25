3f66d2a36b396f15b12f75ab49e649f0c491a959
ackphht <ackphht@outlook.com>
2023-06-24 20:39:08 -0500

    ackfetch.ps1: add

---
+++ ackfetch.ps1
@@ -0,0 +1,12 @@
+﻿#Requires -Version 5.1
+
+[CmdletBinding(SupportsShouldProcess=$true)]
+param()
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+Set-StrictMode -Version 'Latest'
+
+. ./populateSystemData.ps1
+Get-OSDetails |
+	Select-Object Id,Distributor,Description,Codename,Release,KernelVersion |
+	Format-List
