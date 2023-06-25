03d60f41f8e1f5bf0368d32215152084069c234d
ackphht <ackphht@outlook.com>
2023-06-24 21:22:12 -0500

    ackfetch.ps1: sigh

--- ackfetch.ps1
+++ ackfetch.ps1
@@ -6,7 +6,7 @@ param()
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 Set-StrictMode -Version 'Latest'
 
-. ./populateSystemData.ps1
+. $PSScriptRoot/populateSystemData.ps1
 Get-OSDetails |
 	Select-Object Id,Distributor,Description,Codename,Release,KernelVersion |
 	Format-List
