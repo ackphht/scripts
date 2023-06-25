dd60bd8c31ab283f417e0810dc89081fd6228707
ackphht <ackphht@outlook.com>
2023-06-24 23:34:02 -0500

    ackfetch.ps1: add switch to show all props

--- ackfetch.ps1
+++ ackfetch.ps1
@@ -1,12 +1,20 @@
 ﻿#Requires -Version 5.1
 
 [CmdletBinding(SupportsShouldProcess=$true)]
-param()
+param(
+	[switch] [Alias('all')] $showAllProps
+)
 
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 Set-StrictMode -Version 'Latest'
 
+if ($showAllProps) {
+	$propList = @('*')
+} else {
+	$propList = @('Id','Distributor','Description','Codename','Release','KernelVersion')
+}
+
 . $PSScriptRoot/populateSystemData.ps1
 Get-OSDetails |
-	Select-Object Id,Distributor,Description,Codename,Release,KernelVersion |
+	Select-Object -Property $propList |
 	Format-List
