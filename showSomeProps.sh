51757801a3a3b829697261a2a74c9ac51586be22
ackphht <ackphht@outlook.com>
2023-07-05 13:51:01 -0500

    showSomeProps.sh: bleeping java

--- showSomeProps.sh
+++ showSomeProps.sh
@@ -44,7 +44,7 @@ has python3 && echo "python3 = |$(python3 --version | awk '{print $2}')|" || ech
 has git     && echo "git     = |$(git --version | awk '{print $3}')|" || echo 'git     = |<n/a>|'
 has snap    && echo "snap    = |$(snap --version | head --lines 1 | awk '{print $2}')|" || echo "snap    = |<n/a>|"
 has flatpak && echo "flatpak = |$(flatpak --version | awk '{print $2}')|" || echo "flatpak = |<n/a>|"
-has java    && echo "java    = |$(java --version | head --lines 1)|" || echo 'java    = |<n/a>|'	# show whole version line for this one
+has java    && echo "java    = |$(java --version 2>/dev/null | head --lines 1)|" || echo 'java    = |<n/a>|'	# show whole version line for this one
 has perl    && echo "perl    = |$(perl --version | head --lines 2 | tail --lines 1 | sed -E 's/^(.+)\((v[\.0-9]+)\)(.+)$/\2/')|" || echo 'perl    = |<n/a>|'
 has ruby    && echo "ruby    = |$(ruby --version | awk '{print $2}')|" || echo "ruby    = |<n/a>|"
 has go      && echo "go      = |$(go version | awk '{print $3}')|" || echo "go      = |<n/a>|"
