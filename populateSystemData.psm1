c09961cee51ff9828257390513ea934b2c77299f
ackphht <ackphht@outlook.com>
2023-07-24 22:46:24 -0500

    populateSystemData.psm1: Get-Module needs the extension but Import-Module does not; ffs

--- populateSystemData.psm1
+++ populateSystemData.psm1
@@ -7,9 +7,9 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
 # this script also gets symlinked as setUpSystem.00.SystemData for copying to other systems, so the ackPoshHelpers.ps1 might, too (ugh):
-if ([bool](Get-Module -Name "$PSScriptRoot/ackPoshHelpers" -ListAvailable)) {
+if ([bool](Get-Module -Name "$PSScriptRoot/ackPoshHelpers.psd1" -ListAvailable)) {
 	Import-Module -Name "$PSScriptRoot/ackPoshHelpers"
-} elseif ([bool](Get-Module -Name "setUpSystem.00.Helpers" -ListAvailable)) {
+} elseif ([bool](Get-Module -Name "setUpSystem.00.Helpers.psd1" -ListAvailable)) {
 	Import-Module -Name "$PSScriptRoot/setUpSystem.00.Helpers"
 }
 
