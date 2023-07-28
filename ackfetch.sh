4a503459380cf87b74aa431c8b5e8f75798603ad
ackphht <ackphht@outlook.com>
2023-07-28 12:05:23 -0500

    ackfetch.sh: update to just use the posh scripts

--- ackfetch.sh
+++ ackfetch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 doIt() {
-	pwsh -c "& { . ~/scripts/populateSystemData.ps1; \
-					Get-OSDetails | \
-						Select-Object Id,Distributor,Description,Codename,Release,KernelVersion | \
-						Format-List }"
+	#pwsh -c "& { . ~/scripts/populateSystemData.ps1; \
+	#				Get-OSDetails | \
+	#					Select-Object Id,Distributor,Description,Codename,Release,KernelVersion | \
+	#					Format-List }"
+	pwsh -file ~/scripts/ackfetch.ps1
 }
 
 which pwsh >/dev/null 2>&1 && doIt || echo "WARNING: powershell not found"
