1f45604f7442c592ea044781d9c4a8c0f627d920
ackphht <ackphht@outlook.com>
2023-08-03 15:52:48 -0500

    PSReadLineProfile.ps1: rearrange to only load a couple things for Windows (don't work on others)

--- PSReadLineProfile.ps1
+++ PSReadLineProfile.ps1
@@ -5,6 +5,8 @@ using namespace System.Management.Automation.Language
 
 $mod = Import-Module -Name PSReadLine -PassThru
 
+$_isWindows = ($PSEdition -ne 'Core' -or $IsWindows)
+
 Set-PSReadLineOption -EditMode Windows
 if ($mod.Version -gt ([System.Version]'2.2')) {
 	Set-PSReadLineOption -PredictionViewStyle ListView
@@ -20,50 +22,80 @@ Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineKeyHandler -Key 'UpArrow' -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key 'DownArrow' -Function HistorySearchForward
 
-# This key handler shows the entire or filtered history using Out-GridView. The
-# typed text is used as the substring pattern for filtering. A selected command
-# is inserted to the command line without invoking. Multiple command selection
-# is supported, e.g. selected by Ctrl + Click.
-Set-PSReadLineKeyHandler -Key 'F7' `
-						-BriefDescription History `
-						-LongDescription 'Show command history' `
-						-ScriptBlock {
-	$pattern = $null
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
-	if ($pattern) {
-		$pattern = [regex]::Escape($pattern)
-	}
-
-	$history = [System.Collections.ArrayList]@(
-		$last = ''; $lines = '';
-		foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
-			if ($line.EndsWith('`')) {
-				$line = $line.Substring(0, $line.Length - 1)
-				$lines = if ($lines) { "$lines`n$line" } else { $line }
-				continue
-			}
-			if ($lines) {
-				$line = "$lines`n$line"
-				$lines = ''
-			}
-			if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
-				$last = $line
-				$line
-			}
-		}
-	)
-	$history.Reverse()
-
-	$command = $history | Out-GridView -Title History -PassThru
-	if ($command) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-		[Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
-	}
-}
-
 # CaptureScreen is good for blog posts or email showing a transaction
 # of what you did when asking for help or demonstrating a technique.
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
+
+if ($_isWindows) {
+
+	# This key handler shows the entire or filtered history using Out-GridView. The
+	# typed text is used as the substring pattern for filtering. A selected command
+	# is inserted to the command line without invoking. Multiple command selection
+	# is supported, e.g. selected by Ctrl + Click.
+	Set-PSReadLineKeyHandler -Key 'F7' `
+							-BriefDescription History `
+							-LongDescription 'Show command history' `
+							-ScriptBlock {
+		$pattern = $null
+		[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+		if ($pattern) {
+			$pattern = [regex]::Escape($pattern)
+		}
+
+		$history = [System.Collections.ArrayList]@(
+			$last = ''; $lines = '';
+			foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
+				if ($line.EndsWith('`')) {
+					$line = $line.Substring(0, $line.Length - 1)
+					$lines = if ($lines) { "$lines`n$line" } else { $line }
+					continue
+				}
+				if ($lines) {
+					$line = "$lines`n$line"
+					$lines = ''
+				}
+				if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
+					$last = $line
+					$line
+				}
+			}
+		)
+		$history.Reverse()
+
+		$command = $history | Out-GridView -Title History -PassThru
+		if ($command) {
+			[Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+			[Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+		}
+	}
+
+	# F1 for help on the command line - naturally
+	Set-PSReadLineKeyHandler -Key 'F1' `
+							-BriefDescription CommandHelp `
+							-LongDescription 'Open the help window for the current command' `
+							-ScriptBlock {
+		param($key, $arg)
+		$ast = $null; $tokens = $null; $errors = $null; $cursor = $null;
+		[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+		$commandAst = $ast.FindAll( {
+				$node = $args[0]
+				$node -is [CommandAst] -and $node.Extent.StartOffset -le $cursor -and $node.Extent.EndOffset -ge $cursor
+			}, $true) | Select-Object -Last 1
+
+		if ($commandAst -ne $null) {
+			$commandName = $commandAst.GetCommandName()
+			if ($commandName -ne $null) {
+				$command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
+				if ($command -is [AliasInfo]) {
+					$commandName = $command.ResolvedCommandName
+				}
+				if ($commandName -ne $null) {
+					Get-Help $commandName -ShowWindow
+				}
+			}
+		}
+	}
+}
 
 #region Smart Insert/Delete
 # The next four key handlers are designed to make entering matched quotes
@@ -272,34 +304,6 @@ Set-PSReadLineKeyHandler -Key 'Alt+%' `
 					# adjust by the difference in length
 					$startAdjustment += ($resolvedCommand.Length - $length)
 				}
-			}
-		}
-	}
-}
-
-# F1 for help on the command line - naturally
-Set-PSReadLineKeyHandler -Key 'F1' `
-						-BriefDescription CommandHelp `
-						-LongDescription 'Open the help window for the current command' `
-						-ScriptBlock {
-	param($key, $arg)
-
-	$ast = $null; $tokens = $null; $errors = $null; $cursor = $null;
-	[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
-	$commandAst = $ast.FindAll( {
-			$node = $args[0]
-			$node -is [CommandAst] -and $node.Extent.StartOffset -le $cursor -and $node.Extent.EndOffset -ge $cursor
-		}, $true) | Select-Object -Last 1
-
-	if ($commandAst -ne $null) {
-		$commandName = $commandAst.GetCommandName()
-		if ($commandName -ne $null) {
-			$command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
-			if ($command -is [AliasInfo]) {
-				$commandName = $command.ResolvedCommandName
-			}
-			if ($commandName -ne $null) {
-				Get-Help $commandName -ShowWindow
 			}
 		}
 	}
