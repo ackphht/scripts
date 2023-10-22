c6e8c21e4c5c3234cfc40f82235fcbe87436dabf
ackphht <ackphht@outlook.com>
2023-10-22 11:40:46 -0500

    ackfetch.sh: can pass in -a flag

--- ackfetch.sh
+++ ackfetch.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-# TODO: how do we pass in a flag for the '-a' or '--showAllProps' option?
+showAll=""
+while [ $# -ne 0 ]; do
+	name="$1"
+	case "$name" in
+		-a|--show[Aa]ll)
+			showAll="-a"
+			;;
+		-?|--?|-h|--help|-[Hh]elp)
+			script_name="$(basename "$0")"
+			echo "show system info"
+			echo "Usage: $script_name [-a|--showAll]"
+			echo "       $script_name -h|-?|--help"
+			echo ""
+			echo "$script_name dumps out some system details, kind of like neofetch or screenfetch"
+			echo ""
+			echo "Options:"
+			echo "  -a,--showAll              show all properties; default is a short list"
+			echo "  -?,--?,-h,--help,-Help    shows this help message"
+			echo ""
+			exit 0
+			;;
+		*)
+			echo ""
+			echo "Unknown argument \"$name\""
+			exit 1
+			;;
+	esac
+	shift
+done
 
 doItPosh() {
-	pwsh -file ~/scripts/ackfetch.ps1
+	pwsh -file ~/scripts/ackfetch.ps1 $showAll
 }
 
 doItPython() {
-	python3 ~/scripts/ackfetch.py
+	python3 ~/scripts/ackfetch.py $showAll
 }
 
 (which python3 >/dev/null 2>&1 && doItPython) || (which pwsh >/dev/null 2>&1 && doItPosh) || echo "WARNING: neither powershell nor python were found"
