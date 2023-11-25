0c69c4e6f67b105af7140679e294a254eebb0697
ackphht <ackphht@outlook.com>
2023-11-25 17:45:41 -0600

    ackShellStuff.sh: and another quoting problem

--- ackShellStuff.sh
+++ ackShellStuff.sh
@@ -13,7 +13,7 @@ if [[ -z "$currShell" ]]; then
 		Darwin) currShell=$(ps -p $$ -o command=) ;;
 		MINGW*) currShell=$0 ;;		# for git's bash; doesn't support ps -o
 	esac
-elif [[ $currShell == 'busybox' ]]; then
+elif [[ "$currShell" == "busybox" ]]; then
 	currShell=$0		# see if this is more accurate
 fi
 if [[ "${currShell:0:1}" == "/" ]]; then currShell=$(basename $currShell); fi	# in case got a full path
