ce77828bb7a621ccf62f244e3d2eca94cac98464
ackphht <ackphht@outlook.com>
2023-12-23 16:14:16 -0600

    ackShellStuff.sh: derp

--- ackShellStuff.sh
+++ ackShellStuff.sh
@@ -183,7 +183,7 @@ if [[ -x /usr/bin/btrfs || -x /usr/sbin/btrfs ]]; then
 	fi
 fi
 
-if hasCmd pwsh && test -f ~/scripts/zeroLinuxFreeSpace.ps1 && alias zx='sudo $(which pwsh) -f ~/scripts/zeroLinuxFreeSpace.ps1' || true
+hasCmd pwsh && test -f ~/scripts/zeroLinuxFreeSpace.ps1 && alias zx='sudo $(which pwsh) -f ~/scripts/zeroLinuxFreeSpace.ps1' || true
 
 # default prompt in case oh-my-posh (below) isn't installed
 case $currShell in
