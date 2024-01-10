9615d2454e34f4edb093f27fd2691c73f9fbfffa
ackphht <ackphht@outlook.com>
2024-01-10 15:59:36 -0600

    installOhMyPosh.py: refactor to make it a little easier to follow

--- installOhMyPosh.py
+++ installOhMyPosh.py
@@ -1,7 +1,8 @@
 #!python3
 # -*- coding: utf-8 -*-
 
-import sys, os, pathlib, platform, stat, argparse, shutil, subprocess, urllib.request
+import sys, os, pathlib, platform, stat, argparse, shutil, urllib.request
+from collections import namedtuple
 from ackPyHelpers import LogHelper, GithubRelease, Version, RunProcessHelper
 
 PyScript = os.path.abspath(__file__)
@@ -21,61 +22,72 @@ def main():
 			'_pydevd_bundle' not in sys.modules:	# so if we're on windows and working in VSCode (or other editor?), rest of file won't be grayed out; there's also 'debugpy' that's MS specific ??:
 		raise RuntimeError("this script is only for Linux and FreeBSD")
 
+	ompInfo = initOmpInfo(osPlatform, osArch, forceInstall, testMode)
+	if ompInfo.isUpToDate:
+		return	# function logs everything, so just return
+
+	downloadOmp(ompInfo, testMode)
+	disableUpdateCheck(ompInfo, testMode)
+
+OmpInfo = namedtuple("OmpInfo", ["ompBinPath", "isUpToDate", "installedVersion", "latestVersion", "downloadUrl"])
+def initOmpInfo(osPlatform: str, osArch: str, forceInstall: bool, whatIf: bool) -> OmpInfo:
+	ompFilename = f"posh-{osPlatform}-{osArch}"
+	downloadUrl = f"https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/{ompFilename}"
+	ompInfo = OmpInfo(getBinPath(osPlatform, whatIf), False, getCurrentOhMyPoshVer(), Version.zeroVersion(), downloadUrl)
+	LogHelper.Verbose(f"current installed version: |{ompInfo.installedVersion}|")
+	if not ompInfo.installedVersion.isZeroVersion:
+		if not forceInstall:
+			gh = GithubRelease.GetLatestRelease("JanDeDobbeleer", "oh-my-posh")
+			LogHelper.Verbose(f"latest release info: tag = |{gh.tag}|, published at |{gh.published}|")
+			ompInfo = ompInfo._replace(latestVersion=Version.parseVersionString(cleanUpVersion(gh.tag)))
+			if ompInfo.latestVersion > ompInfo.installedVersion:
+				LogHelper.Message(f"installing newer version of OhMyPosh: v{ompInfo.latestVersion} (current version = v{ompInfo.installedVersion})")
+				for a in gh.assets:	# see if we can save them a redirect
+					if a.name == ompFilename:
+						ompInfo = ompInfo._replace(downloadUrl=a.downloadUrl)
+						break
+			else:
+				LogHelper.Message(f"currently installed OhMyPosh is already the latest version: v{ompInfo.installedVersion}")
+				ompInfo = ompInfo._replace(isUpToDate=True)
+		else:
+			LogHelper.Message(f"force mode enabled, ignoring checks and installing latest OhMyPosh (current version = v{ompInfo.installedVersion})")
+	else:
+		LogHelper.Message("no current Oh-My-Posh found, installing latest version")
+	return ompInfo
+
+def downloadOmp(ompInfo: OmpInfo, whatIf: bool) -> None:
+	LogHelper.Verbose(f"writing file |{ompInfo.ompBinPath}| from url |{ompInfo.downloadUrl}|")
+	with urllib.request.urlopen(ompInfo.downloadUrl) as resp:
+		if not whatIf:
+			with open(ompInfo.ompBinPath, "wb") as f:
+				f.write(resp.read())
+		else:
+			LogHelper.WhatIf(f"writing download to file |{ompInfo.ompBinPath}|")
+	# set execute permission:
+	if not whatIf:
+		os.chmod(ompInfo.ompBinPath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IROTH)
+	else:
+		LogHelper.WhatIf(f'setting execute permission on file "{ompInfo.ompBinPath}"')
+
+def disableUpdateCheck(ompInfo: OmpInfo, whatIf: bool) -> None:
+	# make sure the notify thing is off:
+	if not whatIf:
+		result = RunProcessHelper.runProcess([ompInfo.ompBinPath, "disable", "notice"])
+		if result.exitCode != 0:
+			LogHelper.Error(f"error running oh-my-posh disable notice: exit code = {result.exitCode}{os.linesep}{result.getCombinedStdoutStderr()}")
+	else:
+		LogHelper.WhatIf(f'running command "{ompInfo.ompBinPath} disable notice"')
+
+def getBinPath(osPlatform: str, whatIf: bool):
 	binFldr = ".local/bin" if osPlatform == "linux" else "bin"
 	binFldr = pathlib.Path(os.path.expanduser(f"~/{binFldr}"))
 	if not binFldr.exists():
 		LogHelper.Verbose(f"creating bin folder |{binFldr}|")
-		if not testMode:
+		if not whatIf:
 			binFldr.mkdir(parents=True)
 		else:
 			LogHelper.WhatIf(f"creating bin folder |{binFldr}|")
-	outfile = binFldr / "oh-my-posh"
-
-	ompFilename = f"posh-{osPlatform}-{osArch}"
-	downloadUrl = f"https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/{ompFilename}"
-	print("")
-	if not forceInstall:
-		currVer = getCurrentOhMyPoshVer()
-		LogHelper.Verbose(f"current installed version: |{currVer}|")
-		if not currVer.isZeroVersion:
-			gh = GithubRelease.GetLatestRelease("JanDeDobbeleer", "oh-my-posh")
-			LogHelper.Verbose(f"latest release info: tag = |{gh.tag}|, published at |{gh.published}|")
-			latestVer = Version.parseVersionString(cleanUpVersion(gh.tag))
-			if latestVer > currVer:
-				LogHelper.Message(f"installing newer version of OhMyPosh: v{latestVer} (current version = v{currVer})")
-				for a in gh.assets:	# see if we can save them a redirect
-					if a.name == ompFilename:
-						downloadUrl = a.downloadUrl
-						break
-			else:
-				LogHelper.Message(f"currently installed OhMyPosh is already the latest version: v{currVer}")
-				return 0	# nothing else to do
-		else:
-			LogHelper.Message("no current Oh-My-Posh found, installing latest version")
-	else:
-		LogHelper.Message("force mode enabled, ignoring checks and installing latest OhMyPosh")
-
-	LogHelper.Verbose(f"writing file |{outfile}| from url |{downloadUrl}|")
-	with urllib.request.urlopen(downloadUrl) as resp:
-		if not testMode:
-			with open(outfile, "wb") as f:
-				f.write(resp.read())
-		else:
-			LogHelper.WhatIf(f"writing download to file |{outfile}|")
-
-	# set execute permission:
-	if not testMode:
-		os.chmod(outfile, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IROTH)
-	else:
-		LogHelper.WhatIf(f'setting execute permission on file "{outfile}"')
-
-	# make sure the notify thing is off:
-	if not testMode:
-		result = RunProcessHelper.runProcess([outfile, "disable", "notice"])
-		if result.exitCode != 0:
-			LogHelper.Error(f"error running oh-my-posh disable notice: exit code = {result.exitCode}{os.linesep}{result.getCombinedStdoutStderr()}")
-	else:
-		LogHelper.WhatIf(f'running command "{outfile} disable notice"')
+	return binFldr / "oh-my-posh"
 
 def cleanUpVersion(ver : str) -> str:
 	if ver:
