1ada813799cb4ac904200494ed16090605ef682e
ackphht <ackphht@outlook.com>
2024-01-25 11:56:02 -0600

    showCharacters.ps1: rearrange a little

--- showCharacters.ps1
+++ showCharacters.ps1
@@ -9,8 +9,10 @@ $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 Set-StrictMode -Version 'Latest'
 
 Write-Output ''
-Write-Output 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789'
-Write-Output '!?@#$%^&* ;: `''"‘’“” ~-_=+ /\|¦ () [] {} <> oO0 iIlL1 g9qCGQ ~-+=>'
+Write-Output 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+Write-Output 'abcdefghijklmnopqrstuvwxyz'
+Write-Output '0123456789 oO0 IiLl1 g9qCGQ'
+Write-Output '!?@#$%^&* ;: `''"‘’“” ~-_=+ /\|¦ () [] {} <> ~-+=>'
 Write-Output 'Áá Ää Åå Ææ Çç Éé Ùù Ïï İı Ññ ß '
 if ($includeNerdFontChars) {
 	Write-Output '<TBD>'
