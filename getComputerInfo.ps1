def0daf59785b933a2420f67c1edee06b552eb0d
ackphht <ackphht@outlook.com>
2024-06-07 16:04:18 -0500

    getComputerInfo.ps1: erorder few columns; add way to sort some things

--- getComputerInfo.ps1
+++ getComputerInfo.ps1
@@ -36,14 +36,14 @@ function Main {
 	_dumpCimProperties -cn 'Chassis' -a:$writeAll -p  @('Manufacturer', @{ name='Name'; expression={ Coalesce -object $_ -props @('Name', 'Description', 'Caption') }; }, 'SerialNumber') -wmiClass 'SystemEnclosure'
 	_dumpCimProperties -cn 'Processor' -a:$writeAll -p  @('Manufacturer', 'Name', 'Description', 'ProcessorId', @{ name='Architecture'; expression={ MapCimProcArch $_.Architecture }; }, 'NumberOfCores', 'NumberOfLogicalProcessors', 'AddressWidth', 'DataWidth', 'MaxClockSpeed', 'SocketDesignation', 'L2CacheSize', 'L3CacheSize', 'Family', 'Level')
 	_dumpCimProperties -cn 'OperatingSystem' -a:$writeAll -p  @('Manufacturer', 'Caption', 'Description', 'Version', 'BuildNumber', 'OSArchitecture', @{ name='OperatingSystemSKU'; expression={ MapCimOsSku -cimOsSku $_.OperatingSystemSKU -cimOsCaption $_.Caption }; }, 'CurrentTimeZone', 'InstallDate', 'LastBootUpTime')
-	_dumpCimProperties -cn 'DiskDrive' -a:$writeAll -p  @('Manufacturer', 'Model', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, 'Status', 'Index', 'CapabilityDescriptions', 'InterfaceType', 'FirmwareRevision', 'SerialNumber', 'MediaType', 'Partitions', 'PNPDeviceID')
+	_dumpCimProperties -cn 'DiskDrive' -a:$writeAll -p  @('Manufacturer', 'Model', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, 'MediaType', 'Partitions', 'Status', 'Index', 'CapabilityDescriptions', 'InterfaceType', 'FirmwareRevision', 'SerialNumber', 'PNPDeviceID') -sortBy 'Index'
+	_dumpCimProperties -cn 'LogicalDisk' -a:$writeAll -p  @('Name', 'Description', 'FileSystem', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, @{ name='FreeSpace'; expression={ GetFriendlyBytes -value $_.FreeSpace }; }, 'VolumeName', 'VolumeSerialNumber') -sortBy 'Name'
+	_dumpCimProperties -cn 'DiskPartition' -a:$writeAll -p  @('Name', 'Type', 'Bootable', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, 'Index', 'DiskIndex', 'NumberOfBlocks', 'BlockSize', 'StartingOffset') -sortBy 'Index','DiskIndex'
 	_dumpCimProperties -cn 'CDROMDrive' -a:$writeAll -p  @('Manufacturer', 'Name', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, 'Status', 'CapabilityDescriptions', 'MfrAssignedRevisionLevel', 'SerialNumber', 'MediaType', 'PNPDeviceID')
-	_dumpCimProperties -cn 'LogicalDisk' -a:$writeAll -p  @('Name', 'Description', 'FileSystem', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, @{ name='FreeSpace'; expression={ GetFriendlyBytes -value $_.FreeSpace }; }, 'VolumeName', 'VolumeSerialNumber')
-	_dumpCimProperties -cn 'DiskPartition' -a:$writeAll -p  @('Name', 'Type', 'Bootable', @{ name='Size'; expression={ GetFriendlyBytes -value $_.Size }; }, 'DiskIndex', 'Index', 'NumberOfBlocks', 'BlockSize', 'StartingOffset')
 	if ($script:cimAvailable) {		# no equivalent for WMI (?)
-		_dumpCimProperties -cn 'StorageVolume' -a:$writeAll -p  @('Name' ,'DriveLetter', 'Label', 'DeviceId', 'FileSystem', @{ name='Capacity'; expression={ GetFriendlyBytes -value $_.Capacity }; }, @{ name='FreeSpace'; expression={ GetFriendlyBytes -value $_.FreeSpace }; }, 'BlockSize', 'SerialNumber')
+		_dumpCimProperties -cn 'StorageVolume' -a:$writeAll -p  @('Name' ,'DriveLetter', 'Label', 'FileSystem', @{ name='Capacity'; expression={ GetFriendlyBytes -value $_.Capacity }; }, @{ name='FreeSpace'; expression={ GetFriendlyBytes -value $_.FreeSpace }; }, 'BlockSize', 'SerialNumber', 'DeviceId')
 	}
-	_dumpCimProperties -cn 'VideoController' -a:$writeAll -p @('Name', 'VideoProcessor', 'DriverVersion', 'PNPDeviceID', 'Status', @{ name='AdapterRAM'; expression={ GetFriendlyBytes -value $_.AdapterRAM }; }, 'VideoModeDescription', 'CurrentRefreshRate')
+	_dumpCimProperties -cn 'VideoController' -a:$writeAll -p @('Name', 'VideoProcessor', 'DriverVersion', 'Status', @{ name='AdapterRAM'; expression={ GetFriendlyBytes -value $_.AdapterRAM }; }, 'VideoModeDescription', 'CurrentRefreshRate', 'PNPDeviceID')
 }
 
 function _dumpCimProperties {
@@ -51,12 +51,16 @@ function _dumpCimProperties {
 		[Parameter(Mandatory=$true)] [Alias('cn', 'n')] [string] $className,
 		[Parameter(Mandatory=$true)] [Alias('p')] [PSObject[]] $stdPropList,
 		[switch] [Alias('a')] $writeAllProps,
-		[string] $wmiClass
+		[string] $wmiClass,
+		[string[]] $sortBy
 	)
 	if ($script:cimAvailable) {
 		$info = Get-CimInstance -ClassName ('CIM_' + $className)
 	} else {
 		$info = Get-WmiObject -Class ('Win32_' + $(if ($wmiClass) { $wmiClass } else { $className }))
+	}
+	if ($sortBy) {
+		$info = $info | Sort-Object -Property $sortBy
 	}
 	if ($info) {
 		_writeHeader $className
