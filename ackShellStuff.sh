31babd2b224f469b3b8f7a4b3ce73dbfd3ea08d4
ackphht <ackphht@outlook.com>
2024-07-03 23:27:31 -0500

    ackShellStuff.sh: add aptuu for zypper for dist-upgrade

--- ackShellStuff.sh
+++ ackShellStuff.sh
@@ -109,6 +109,7 @@ elif hasCmd zypper; then
 	alias aptr='sudo zypper refresh' # --force'	# if output is piped into, e.g. grep, it displays a warning about not having a 'stable CLI interface', 'use with caution'; ???
 	alias aptul='sudo zypper list-updates'		# no idea why this requires sudo now...
 	alias aptu='sudo zypper update --no-confirm --no-recommends'
+	alias aptuu='sudo zypper dist-upgrade --no-recommends'	# --no-confirm
 	#alias aptc='sudo zypper remove --clean-deps && sudo zypper clean --all'
 	alias aptc='sudo zypper clean' # --all'
 	alias apts='zypper search'
