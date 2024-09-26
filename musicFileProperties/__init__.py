e8c173ff9e8298507dad7a379ddc82357387ad49
ackphht <ackphht@outlook.com>
2024-09-26 11:03:10 -0500

    musicFileProperties.py: move to folder to make proper lib; add an init script

---
+++ musicFileProperties/__init__.py
@@ -0,0 +1,17 @@
+"""
+just some helper classes for my music files
+"""
+
+__author__ = "AckWare"
+__version__ = "1.0.0"
+
+__all__ = [ 'MusicFileProperties', 'Mp4TagNames', ]
+
+import sys
+
+# The very first thing we do is give a useful error if someone is
+# running this code under version of Python we don't want to support:
+if sys.version_info < (3, 9):
+    raise ImportError('This module only supports v3.9 and up of python.')
+
+from .musicFileProperties import MusicFileProperties, Mp4TagNames
