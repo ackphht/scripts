08c9159afe2a2c10fc63690cea099ef3f3e01059
ackphht <ackphht@outlook.com>
2024-10-04 00:20:02 -0500

    musicFileProperties init: remove TagMapper from being an external module member

--- musicFileProperties/__init__.py
+++ musicFileProperties/__init__.py
@@ -5,7 +5,7 @@ just some helper classes for my music files
 __author__ = "AckWare"
 __version__ = "1.0.0"
 
-__all__ = [ 'MusicFileProperties', 'TagNames', 'TagMapper', 'Mp4TagNames', ]
+__all__ = [ 'MusicFileProperties', 'TagNames', 'Mp4TagNames', ]
 
 import sys
 
@@ -15,6 +15,5 @@ if sys.version_info < (3, 9):
 	raise ImportError('This module only supports v3.9 and up of python.')
 
 from .musicFileProperties import MusicFileProperties
-from .tagMapper import TagMapper
 from .tagNames import TagNames
 from .mp4TagNames import Mp4TagNames
