54a488f6cab9790fa3c574864b3a63bb8e9ec05e
ackphht <ackphht@outlook.com>
2025-01-11 12:22:12 -0600

    cleanUpUnwantedStuff.ps1: when cleaning up path vars, also remove trailing slashes (not needed, keep everything the same, and enable more dupes to be detected)

--- cleanUpUnwantedStuff.ps1
+++ cleanUpUnwantedStuff.ps1
@@ -1371,6 +1371,10 @@ function NormalizePathValue {
 	$value = [System.Environment]::ExpandEnvironmentVariables($value)			# expand it all the way out so we can replace with preferred stuff
 	WriteVerboseMessage 'expanded value = |{0}|' $value -continuation
 	$value = Convert-Path -LiteralPath $value -ErrorAction SilentlyContinue		# get rid of any relative paths
+	while ($value.EndsWith([System.IO.Path]::DirectorySeparatorChar) -or $value.EndsWith([System.IO.Path]::AltDirectorySeparatorChar)) {
+		# remove trailing backslashes
+		$value = $value.Substring(0, $value.Length - 1)
+	}
 	WriteVerboseMessage 'converted value = |{0}|' $value -continuation
 	$value = ReplaceWithEnvVars -value $value
 	WriteVerboseMessage 'returning value = |{0}|' $value -continuation
