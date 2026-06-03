1e5cf3cccb02af984c3e88fd74c21c3fd55ba80d
ackphht <ackphht@outlook.com>
2026-06-03 11:30:37 -0500

    00.installWinget.ps1: import common stuff; use Test-UsableVersionOfWinget; use better verbose logging helper

--- systemSetup/00.installWinget.ps1
+++ systemSetup/00.installWinget.ps1
@@ -11,13 +11,18 @@ param()
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 Set-StrictMode -Version 'Latest'
 
+. $PSScriptRoot/00.common.ps1
+
 $minVersion = [System.Version]'10.0.16299'	# Win10 1709, lowest supported version for winget
 if ([System.Environment]::OSVersion.Version -lt $minVersion) {
 	Write-Warning "unsupported OS: Windows Package Manager requires Windows 10 1709 (build 16299) or higher"
 	return
 }
 
-# TODO?: check if it's already installed with a suitable version ??
+if (Test-UsableVersionOfWinget) {
+	WriteStatusMessage -message 'acceptable version of winget already installed; exiting'
+	return
+}
 
 $windowsPkgMngrFilename = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 # should be able to use System.Runtime.InteropServices.RuntimeInformation.OSArchitecture back to .NET 4.7.1 per docs,
@@ -42,19 +47,19 @@ try {
 
 	$url = "https://aka.ms/${vclibsFilename}"
 	Write-Host 'downloading VCLibs dependency' -ForegroundColor DarkCyan
-	Write-Verbose "downloading VCLibs dependency: url = |$url|`n    output filename = |$vclibsTempFile|"
+	WriteVerboseMessage -message 'downloading VCLibs dependency: url = |{0}|`n    output filename = |{1}|' -formatParams $url, $vclibsTempFile
 	Invoke-WebRequest -Method GET -Uri $url -OutFile $vclibsTempFile
 
 	$url = "https://github.com/microsoft/winget-cli/releases/latest/download/${windowsPkgMngrFilename}"
 	Write-Host 'downloading windows package manager' -ForegroundColor DarkCyan
-	Write-Verbose "downloading windows package manager: url = |$url|`n    output filename = |$windowsPkgMngrTempFile|"
+	WriteVerboseMessage -message 'downloading windows package manager: url = |{0}|`n    output filename = |{1}|' -formatParams $url, $windowsPkgMngrTempFile
 	Invoke-WebRequest -Method GET -Uri $url -OutFile $windowsPkgMngrTempFile
 
 	Write-Host 'installing VCLibs dependency' -ForegroundColor DarkCyan
-	Write-Verbose "installing package |$vclibsFilename|"
+	WriteVerboseMessage -message 'installing package |{0}|' -formatParams $vclibsFilename
 	Add-AppxPackage -Path $vclibsTempFile
 	Write-Host 'installing windows package manager' -ForegroundColor DarkCyan
-	Write-Verbose "installing package |$windowsPkgMngrFilename|"
+	WriteVerboseMessage -message 'installing package |{0}|' -formatParams $windowsPkgMngrFilename
 	Add-AppxPackage -Path $windowsPkgMngrTempFile
 } finally {
 	if (Test-Path -Path $vclibsTempFile -PathType Leaf) { Remove-Item -Path $vclibsTempFile -Force }
